ca6d4b8f277420f9fd9a00121f00d18644488648
Alain Moratalla <realmoratalla@gmail.com>
2021-05-20 13:07:39 +0800

    added default view for desktop and working fetch data

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,19 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Filters = () => {
+import styles from "./Filters.module.scss";
+
+const Filters = ({ filtersData:data, regionFilterOpts, searchInput }) => {
+    const [ isRegionOptionsShown, setIsRegionOptionsShown] = useState(false);
+    const [ regionOptions, setRegionOptions ] = useState([]);
+    const [ selectedRegionOptions, setSelectedRegionOptions ] = useState([]);
+    const [searchKey, setSearchKey] = useState('');
+
+    useEffect(() => {
+        let regions = []
+        data.forEach(country => {
+            if(country.region && !regions.includes(country.region)){
+                regions.push(country.region)
+            }
+        })
+        setRegionOptions(regions);
+        setSelectedRegionOptions(regions);
+
+    },[data])
+
+
+    const selectedRegionOptionsHandler = (e,opt) => {
+        (!selectedRegionOptions.includes(opt)) 
+            ? setSelectedRegionOptions([...selectedRegionOptions, opt])            
+            : setSelectedRegionOptions(selectedRegionOptions.filter(selectedregion => selectedregion !== opt))
+        e.preventDefault()
+    }
+
+    const selectedAllRegionsHandler = (e) => {
+        (selectedRegionOptions.sort().toString() === regionOptions.sort().toString()) ? setSelectedRegionOptions([]) : setSelectedRegionOptions([...regionOptions]);
+        e.preventDefault()
+    }
+
+    const submitRegionFilterHandler = () =>{
+        setIsRegionOptionsShown(!isRegionOptionsShown);
+        regionFilterOpts(selectedRegionOptions)
+	}
+
+    
+	const onInputSearchKeyHandler = (e) => {
+		searchInput(e.target.value);
+		setSearchKey(e.target.value);
+	}
+
     return (
-        <section>
-            <div>
+        <section className={styles.container__filters}>
+            <div className={styles.container__search}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                    <path d="M9.516 14.016q1.875 0 3.188-1.313t1.313-3.188-1.313-3.188-3.188-1.313-3.188 1.313-1.313 3.188 1.313 3.188 3.188 1.313zm6 0l4.969 4.969-1.5 1.5-4.969-4.969v-.797l-.281-.281q-1.781 1.547-4.219 1.547-2.719 0-4.617-1.875T3.001 9.516t1.898-4.617 4.617-1.898 4.594 1.898 1.875 4.617q0 .984-.469 2.227t-1.078 1.992l.281.281h.797z"/>
+                </svg>
                 <label htmlFor="search">Search</label>
-                <input type="text" name="search" id="search"/>
-                <img src="" alt="Search"/>
+                <input type="text" name="search" id="search"
+                    placeholder={"Search for a country..."}
+                    value={searchKey}
+                    onChange={onInputSearchKeyHandler}
+                />
             </div>
-            <div>
-                <select>
-                <option></option>
-                <option></option>
-                <option></option>
-                </select>
+            <div className={styles.container__selectfilter}>
+                <div className={styles.selectbox}
+                    onClick={()=> setIsRegionOptionsShown(!isRegionOptionsShown)}
+                >
+                    <label htmlFor="filter">Filter by Region</label>
+                    <select className={styles.overlap} placeholder="Filter by Region" name="filter" id="filter">
+                    </select>
+                    <div className={styles.overselect}></div>
+                </div>
+                <div className={`${styles.checkboxes}`}
+                    style={{display: isRegionOptionsShown ? 'block':'none'}}
+                >
+                    <div className={styles.checkboxes__optgroup}>
+                        <label 
+                            htmlFor="region-showall"
+                            onClick={(e)=> selectedAllRegionsHandler(e)}
+                        >
+                            <input type="checkbox" 
+                                id="region-showall" value={"*"} 
+                                checked={(selectedRegionOptions.sort().toString() === regionOptions.sort().toString())}
+                                onChange={() => {}}
+                            />
+                            <span>(Show All)</span>
+                        </label>
+
+                        {regionOptions.sort().map((opt, i) => (
+                            <label 
+                                htmlFor={opt} key={`region_${opt}-${i}`}
+                                onClick={e => selectedRegionOptionsHandler(e, opt)}
+                            >
+                                <input type="checkbox" 
+                                    id={opt} value={opt}
+                                    onChange={()=> {}}
+                                    checked={(selectedRegionOptions.includes(opt))}
+                                />
+                                <span>{opt}</span>
+                            </label>
+                        ))}
+                    </div>
+                    <div className={styles.filtersubmit}>
+                        <label htmlFor="regionsubmit" 
+                            onClick={()=>{ submitRegionFilterHandler()}}
+                        >
+                            <input type="button" value="Done" id="regionsubmit" />
+                        </label>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,24 +1,99 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Filters from '../../components/Filters/Filters';
+
+import loaderGif from "../../assets/loaders/151.gif";
 
 import styles from './Home.module.scss';
 
+const loader = 
+    (<div className={styles.container__loader}>
+        <span>Resources are Loading...</span>
+        <img src={loaderGif} alt="loader gif"/>
+    </div>)
+
+const noDataFound = 
+    (<div className={styles.container__error}>
+        <h3>No data found.</h3>
+        <span>Something may have went wrong... No data is displayed. Please try reloading.</span>
+    </div>)
+
 const Home = () => {
+    const [ countriesData, setCountriesData ] = useState([]);
+    const [ regionFilters, setRegionFilters ] = useState([]);
+    const [ searchKey, setSearchKey ] = useState('');
+    const [ isLoading, setIsLoading ] = useState(false)
+
+    const fetchCountriesData = async () => {
+        try {
+            setIsLoading(true);
+            const data = await fetch('https://restcountries.eu/rest/v2/all');
+            const json = await data.json();
+            setCountriesData(json)
+            setIsLoading(false)
+        } catch(err){
+            console.log(err)
+            console.error(err)
+            setCountriesData([]);
+            setIsLoading(false);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchCountriesData();
+    },[]);
+
+
+    const loadCountriesContent = useMemo(() => {
+        let countriesarray = [...countriesData];
+        if(searchKey){
+            const cols = countriesarray[0] && Object.keys(countriesarray[0]);
+            countriesarray = countriesarray.filter((row)=> {
+                return cols.some(column => (row[column])? row[column].toString().toLowerCase().indexOf(searchKey.toString().toLowerCase()) >-1 : false)
+            })
+        }
+        return countriesarray.filter(country => regionFilters.length > 0 ? regionFilters.includes(country.region) : true)
+    }, [regionFilters, countriesData, searchKey])
+
+
     return (
         <div className={styles.container__home}>
             <main>
-                <Filters/>
-                <div>
-                    <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    </ul>
+                <Filters filtersData={countriesData} 
+                    regionFilterOpts={(f)=>setRegionFilters(f)} 
+                    searchInput={(s)=>setSearchKey(s)}
+                />
+                <div className={styles.container__countrieslist}>
+                    {isLoading ? loader : loadCountriesContent.length === 0 ? noDataFound :
+                        <ul className={styles.countrieslist}>
+                            { loadCountriesContent.map(country => (
+                                <li className={styles.li__container} key={`${country.name}_${country.demonym}`}>
+                                    <div className={styles.li__countrycard}>
+                                        <div className={styles.countrycard__img }>
+                                            <img src={country.flag} alt={`${country.demonym} flag`} />
+                                        </div>
+                                        <div className={styles.countrycard__details}>
+                                            <h3>{country.name}</h3>
+                                            <ul className={styles.countrycard__details__sub}>
+                                                <li>
+                                                    <h5>Population:</h5>
+                                                    <span>{country.population}</span>
+                                                </li>
+                                                <li>
+                                                    <h5>Region:</h5>
+                                                    <span>{country.region}</span>
+                                                </li>
+                                                <li>
+                                                    <h5>Capital:</h5>
+                                                    <span>{country.capital}</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>)
+                            )}
+                        </ul>
+                    }
                 </div>
             </main>
         </div>
